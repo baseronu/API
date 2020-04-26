19011c4795e12c66a85f6d84cdd6047fcca14a4f
baseronu <onur@umich.edu>
2020-04-25 21:09:39 -0400

    I finished the account summary code
    I finished the account summary code

---
+++ keys.sh
@@ -0,0 +1,4 @@
+export quandl_api_key=7DaywxDnfxjsByfPdnWP
+export PLAID_CLIENT_ID=5e98e90c547cd50015d97ec8
+export PLAID_PUBLIC_KEY=954fcf548b85751da065653b7add92
+export PLAID_SBX_SECRET_KEY=4d449b77c44ec1bf2013cb64349ea9
